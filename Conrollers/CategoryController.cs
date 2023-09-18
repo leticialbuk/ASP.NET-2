30f4b9e4a350d0efcecb8a4cd26ae858bf5bfb09
leticialbuk <leticia.albuk@gmail.com>
2023-09-18 12:59:50 -0300

    Criando endpoint Get

---
+++ Conrollers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_2.Conrollers
+{
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("v1/categories")]
+        public async Task<IActionResult> GetAsync(
+            [FromServices]BlogDataContext context) 
+        {
+            var categories = await context.Categories.ToListAsync();   
+            return Ok(categories);    
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+            var category = await context
+                .Categories.
+                FirstOrDefaultAsync(x=>x.Id == id);
+
+            if (category == null) 
+                return NotFound();
+
+            return Ok(category);
+        }
+    }
+}
---
+++ Conrollers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_2.Conrollers
+{
+    [ApiController]
+    [Route ("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet ("")]
+        public IActionResult Get() 
+        { 
+            return Ok();
+        }
+    }
+}
