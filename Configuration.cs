93ed8a7d0cc2650a852a9922302874be9d564586
leticialbuk <leticia.albuk@gmail.com>
2023-09-28 13:40:49 -0300

    estruturando projeto para ter token services

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog_2
+{
+    public static class Configuration
+    {
+        public static string JwtKey { get; set; } = "ZmVkYWY3ZDgNiJyV5JDcTguW";
+    }
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Models;
+
+namespace Blog_2.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user) 
+        {
+        
+        }
+    }
+}
