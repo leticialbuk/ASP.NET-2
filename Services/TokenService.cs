5f1055bfe5530e3e5e5463c0485d792dfb631c73
leticialbuk <leticia.albuk@gmail.com>
2023-09-29 14:56:09 -0300

    gerando token service atrvés de jwt

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,7 @@
 ﻿using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace Blog_2.Services
 {
@@ -6,7 +9,17 @@ namespace Blog_2.Services
     {
         public string GenerateToken(User user) 
         {
-        
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha384Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor); 
+            return tokenHandler.WriteToken(token);
         }
     }
 }
