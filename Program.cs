0e2dc0e401d882aa7dac0c50955f3ff177bdf9a2
leticialbuk <leticia.albuk@gmail.com>
2023-10-05 22:19:22 -0300

    configurando a autenticação e autorização

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
 using Blog.Data;
+using Blog_2;
 using Blog_2.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; //sistema de autenticação padrão aspnet
+}).AddJwtBearer(x => 
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+});
 
 builder
     .Services
@@ -13,5 +33,11 @@ builder.Services.AddDbContext<BlogDataContext>();
 builder.Services.AddTransient<TokenService>(); //cria uma nova instancia e gera um novo token
 
 var app = builder.Build();
+
+app.UseAuthentication (); //sempre usar nessa ordem atenticação/autorização
+app.UseAuthorization();
+
+
 app.MapControllers();
+
 app.Run();
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -8,18 +8,25 @@ namespace Blog_2.Services
 {
     public class TokenService
     {
-        public string GenerateToken(User user) 
+        public string GenerateToken(User user)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
             var tokenDescriptor = new SecurityTokenDescriptor
             {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new (ClaimTypes.Name, "leicialbuk"),
+                    new (ClaimTypes.Role, "admin"),
+                    new ("fruta", "melancia") //exmeplo de claim em token
+                }),
+
                 Expires = DateTime.UtcNow.AddHours(8),
                 SigningCredentials = new SigningCredentials(
                     new SymmetricSecurityKey(key),
                     SecurityAlgorithms.HmacSha384Signature)
             };
-            var token = tokenHandler.CreateToken(tokenDescriptor); 
+            var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
     }
